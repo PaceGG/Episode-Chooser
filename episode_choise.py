db7a4a6dc23bae7e353f4a560cb7bf49b5f41f4a
PaceGG <yura3012004@gmail.com>
2024-05-19 04:35:21 +0500

    2.53

--- episode_choise.py
+++ episode_choise.py
@@ -28,16 +28,16 @@ chat_id = '-1002035302407'
 # DATA
 
 # Официальные назцания игр
-first_game_name = 'Resident Evil 7'
+first_game_name = 'Resident Evil Village'
 second_game_name = 'S.T.A.L.K.E.R. Lost Alpha DC'
 third_game_name = 'SnowRunner'
 
 # Название ярлыков
-first_game_ico = "re7"
+first_game_ico = "re8"
 second_game_ico = 'Lost Alpha Configurator'
 
 # create files:
-# icon.png in icons_new folder
+# ng.png in icons_new folder
 # run mkdir.py
 
 # Пути к файлам видео
--- mkdir.py
+++ mkdir.py
@@ -8,14 +8,26 @@ def create_game_structure(game_name, base_directory):
     episodes_log_path = os.path.join(previews_path, "episodes log.txt")
     with open(episodes_log_path, 'w'):
         pass
+    print("Директории созданы")
 
+def icon_rename(game_name):
+    directory = "D:\\Program Files\\HTML\\Games\\icons_new"
+    variants = ["ng", "new_game", "new game"]
+    
+    for variant in variants:
+        full_path = os.path.join(directory, f"{variant}.png")
+        if os.path.exists(full_path):
+            new_full_path = os.path.join(directory, f"{game_name}.png")
+            os.rename(full_path, new_full_path)
+            print(f"Файл '{full_path}' переименован в '{new_full_path}'")
+            return
+    print("Ни один из файлов не найден")
     
 
 # Название игры
-game_name = "S.T.A.L.K.E.R. Lost Alpha DC"
+game_name = "Resident Evil Village"
 
 
-
+icon_rename(game_name)
 base_directory = "D:/Program Files/Shadow Play"
 create_game_structure(game_name, base_directory)
-print("Директории созданы")
