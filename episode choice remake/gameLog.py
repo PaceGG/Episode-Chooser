d54f1fd10652a427e23bc10d043a03f167c6d604
PaceGG <yura3012004@gmail.com>
2024-08-27 01:04:56 +0500

    add: module jsonLoader refactor: gameLog: json loading functions replaced with jsonLoader module

--- episode choice remake/gameLog.py
+++ episode choice remake/gameLog.py
@@ -1,20 +1,14 @@
-import json
+from jsonLoader import *
+path = "episode choice remake/game_log.json"
+
 from timeFormat import date_position, today, end_of_month
 
-def load_log():
-    with open('episode choice remake/game_log.json', 'r', encoding='utf-8') as f:
-        return json.load(f)
-    
-def save_log(log):
-    with open('episode choice remake/game_log.json', 'w', encoding='utf-8') as f:
-        json.dump(log, f, indent=4)
-
-default = [[[]]*7]*5
+default = [[[], [], [], [], [], [], []],[[], [], [], [], [], [], []],[[], [], [], [], [], [], []],[[], [], [], [], [], [], []],[[], [], [], [], [], [], []]]
 
 def game_log(name):
     week, day = date_position(today())
 
-    log = load_log()
+    log = json_load(path)
 
     if today() > log["next_reset"]:
         log["game_log"]= default
@@ -22,10 +16,7 @@ def game_log(name):
         # calc_statistics(log["game_log"])
 
     log["game_log"][week][day].append(name)
-    save_log(log)
-
-    
-
+    json_save(path, log)
 
 if __name__ == "__main__":
     game_log("Mafia II: Definitive Edition")
---
+++ episode choice remake/jsonLoader.py
@@ -0,0 +1,24 @@
+import json
+
+def json_load(filename):
+    """Load data from a JSON file and return it."""
+    try:
+        with open(filename, 'r') as file:
+            data = json.load(file)
+        return data
+    except FileNotFoundError:
+        print(f"Error: The file {filename} was not found.")
+        return None
+    except json.JSONDecodeError:
+        print(f"Error: Failed to decode JSON from the file {filename}.")
+        return None
+
+@staticmethod
+def json_save(filename, data):
+    """Save data to a JSON file."""
+    try:
+        with open(filename, 'w') as file:
+            json.dump(data, file, indent=4)
+        print(f"Data successfully saved to {filename}.")
+    except Exception as e:
+        print(f"Error: Failed to save data to {filename}. Reason: {e}")
