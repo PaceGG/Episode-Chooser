5b9703d96deb749acfa66ba4117be5c82fd46123
PaceGG <yura3012004@gmail.com>
2024-09-04 07:44:58 +0500

    merge all json files into one fix: missed YTTitle module

--- episode choose remake/YTTitle.py
+++ episode choose remake/YTTitle.py
@@ -1,22 +1,21 @@
-from jsonLoader import *
-
-yt_title_log_path = "episode choose remake/game_log_YTtitle.json"
+from pydata import *
 
 def yt_title_pop():
-    yt_log = json_load(yt_title_log_path)
+    yt_log = pydata_load("game_log_YTTitle")
     try: pop = yt_log.pop(0)
     except: pop = None
-    json_save(yt_title_log_path, yt_log)
+    pydata_save(yt_log, "game_log_YTTitle")
 
     return pop
 
 def add_yt_titles(game_name):
     from episodesManipulate import get_last_object
-    yt_log = json_load(yt_title_log_path)
+    # yt_log = json_load(yt_title_log_path)
+    yt_log = pydata_load("game_log_YTTitle")
     ep_range = get_last_object(game_name)[0]["ep_range"]
 
     for i in range(ep_range[0], ep_range[1] +1):
         yt_log.append(f" • № {i} • {game_name}")
         if i == 1: yt_log.append(game_name)
 
-    json_save(yt_title_log_path, yt_log)
+    pydata_save(yt_log, "game_log_YTTitle")
