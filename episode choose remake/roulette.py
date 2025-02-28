c3249c94b655f33a4a565eb07e56e067299e42f4
PaceGG <yura3012004@gmail.com>
2025-03-01 00:40:18 +0500

    replaced rich with colorama

--- episode choose remake/roulette.py
+++ episode choose remake/roulette.py
@@ -2,9 +2,16 @@ print("Загрузка модуля roulette")
 import random
 import time
 import os
-from rich.console import Console
+from colorama import init
+init()
 
-console = Console()
+def hex_to_rgb(hex_color) -> tuple[int, int, int]:
+    hex_color = hex_color.lstrip('#')
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
+def print_colored_hex(text: str, hex_color: str):
+    r, g, b = hex_to_rgb(hex_color)
+    print(f"\033[38;2;{r};{g};{b}m{text}\033[0m")
 
 def clear_console():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -17,7 +24,7 @@ def print_screen(step, blink=False, color=None):
         prefix = "> " if i == SCREEN_SIZE // 2 else "      "
         element = screen[i].name if not blink or i != SCREEN_SIZE // 2 else " "
         custom_color = screen[i].color if color is None or i == SCREEN_SIZE // 2 else color
-        console.print(prefix + element, style=custom_color)
+        print_colored_hex(prefix + element, hex_color=custom_color)
 
 
 def blink_screen(step):
