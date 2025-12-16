9343ac36f100a7217a42096fe61870bfd473e5f7
PaceGG <yura3012004@gmail.com>
2025-12-16 09:08:02 +0500

    chore: удалены неиспользуемые файлы

--- node_modules/@mui/material/esm/version/index.js
+++
@@ -1,6 +0,0 @@
-export const version = "7.3.2";
-export const major = Number("7");
-export const minor = Number("3");
-export const patch = Number("2");
-export const prerelease = undefined;
-export default version;
