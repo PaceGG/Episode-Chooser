88a4abf607a9347c263d43be5d2d81b4cbdd5fff
PaceGG <yura3012004@gmail.com>
2025-12-16 15:47:18 +0500

    feat: изменен layout

--- episode-chooser-desktop/app/page.tsx
+++ episode-chooser-desktop/app/page.tsx
@@ -2,13 +2,46 @@ import { Box, Stack, Typography } from "@mui/material";
 import DiskSpace from "./components/DiskSpace";
 import VideoInfo from "./components/VideoInfo";
 import GamesInfo from "./components/GamesInfo/GamesInfo";
+import GameCard, { Game } from "./components/GamesInfo/GameCard";
+
+const snowRunner: Game = {
+  color: "#fff000",
+  title: "SnowRunner",
+  time: 120,
+  limit: 0,
+  quote: 0,
+};
+
+const asc: Game = {
+  color: "#f00",
+  title: "Assassin’s Creed Revelations",
+  time: 84,
+  limit: 5,
+  quote: 5,
+  // forced: true,
+};
+
+const silksong: Game = {
+  color: "#00f",
+  title: "Hollow Knight: Silksong",
+  time: 120,
+  limit: -5,
+  quote: 0,
+};
 
 export default function Home() {
   return (
-    <Stack>
+    <Stack
+      flexDirection={"row"}
+      gap={2}
+      flexWrap={"wrap"}
+      justifyContent={"center"}
+      maxWidth={800}
+    >
       <DiskSpace use={33} total={977} videoOnDisk={4} videoToDel={3} />
-      {/* <VideoInfo onDisk={4} toDelete={3} /> */}
-      <GamesInfo />
+      <GameCard game={snowRunner} />
+      <GameCard game={asc} />
+      <GameCard game={silksong} />
     </Stack>
   );
 }
