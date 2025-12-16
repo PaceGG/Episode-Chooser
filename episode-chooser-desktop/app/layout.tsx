a446a1e2ce60c80588059d9ad7c7672fd6899fe6
PaceGG <yura3012004@gmail.com>
2025-12-16 09:42:44 +0500

    feat: кастомный шрифт

--- episode-chooser-desktop/app/layout.tsx
+++ episode-chooser-desktop/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "./providers";
+import { chalet } from "./fonts/fonts";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${chalet.variable} ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Providers>{children}</Providers>
       </body>
