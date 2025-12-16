d0929e07424d7fc5f34074788f602a59daf7e71d
PaceGG <yura3012004@gmail.com>
2025-12-16 15:04:27 +0500

    fix: исправлены цвета в лимитах времени

--- episode-chooser-desktop/app/page.tsx
+++ episode-chooser-desktop/app/page.tsx
@@ -1,14 +1,14 @@
-import { Box, Typography } from "@mui/material";
+import { Box, Stack, Typography } from "@mui/material";
 import DiskSpace from "./components/DiskSpace";
 import VideoInfo from "./components/VideoInfo";
 import GamesInfo from "./components/GamesInfo/GamesInfo";
 
 export default function Home() {
   return (
-    <Box>
+    <Stack direction={"row"}>
       <DiskSpace use={970} total={977} />
       {/* <VideoInfo onDisk={4} toDelete={3} /> */}
       <GamesInfo />
-    </Box>
+    </Stack>
   );
 }
