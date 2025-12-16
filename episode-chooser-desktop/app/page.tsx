cd28b06263fe6cede4b448d82d73265f83ad88f0
PaceGG <yura3012004@gmail.com>
2025-12-16 11:48:06 +0500

    feat: компонент информации про видео на диске

--- episode-chooser-desktop/app/page.tsx
+++ episode-chooser-desktop/app/page.tsx
@@ -1,10 +1,12 @@
 import { Box, Typography } from "@mui/material";
 import DiskSpace from "./components/DiskSpace";
+import VideoInfo from "./components/VideoInfo";
 
 export default function Home() {
   return (
     <Box>
-      <DiskSpace use={970} total={977} />
+      {/* <DiskSpace use={970} total={977} /> */}
+      <VideoInfo onDisk={4} toDelete={3} />
     </Box>
   );
 }
