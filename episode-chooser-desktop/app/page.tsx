90c351415faaa85e66a0f4dd7ea8c35dac701196
PaceGG <yura3012004@gmail.com>
2025-12-16 14:08:50 +0500

    feat: AI дизайн компонента карточки игры

--- episode-chooser-desktop/app/page.tsx
+++ episode-chooser-desktop/app/page.tsx
@@ -1,7 +1,7 @@
 import { Box, Typography } from "@mui/material";
 import DiskSpace from "./components/DiskSpace";
 import VideoInfo from "./components/VideoInfo";
-import GamesInfo from "./components/GamesInfo";
+import GamesInfo from "./components/GamesInfo/GamesInfo";
 
 export default function Home() {
   return (
