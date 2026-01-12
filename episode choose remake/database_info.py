86cb577a66f2b659521052724662364c6a60a20d
PaceGG <yura3012004@gmail.com>
2026-01-12 19:30:42 +0500

    feat: Стилизация консоли и выводимой информации

--- episode choose remake/database_info.py
+++ episode choose remake/database_info.py
@@ -1,4 +1,5 @@
 print("Загрузка модуля database_info")
+import re
 from psutil import disk_usage
 import paths
 from data import Data
@@ -57,12 +58,31 @@ def disk_info(games: list[Game], titles: list):
     info_str = ""
     
     free_space = disk_usage(str(paths.video_dir.drive)).free/1024/1024/1024
-    info_str += f"Место на диске: {free_space:.2f} GB ~ {int(free_space//15)} видео\n"
+    video_space_count = int(free_space//20)
+    info_str += f"{free_space:.2f} GB ~ {video_space_count} видео\n"
 
     disk_video = get_disk_video(games)
-    info_str += f"Видео на диске: {disk_video}\n"
+    del_video = disk_video - len(titles)
+    if disk_video > 0 or del_video > 0:
+        info_str += f"Видео на диске: {disk_video}"
+    if del_video > 0:
+        info_str += f"/{del_video} из них к удалению"
+    if disk_video > 0 or del_video > 0:
+        info_str += "\n"
 
-    info_str += f"Видео к удалению: {disk_video - len(titles)}\n"
+    width = get_strings_width(info_str)
+    if video_space_count <= 4:
+        info_str = hr("! Недостаточно места !".upper(), width=width, color="#ff0000") + info_str
+    elif video_space_count <= 8:
+        info_str = hr("Мало места".upper(), width=width, color="#fdca2d") + info_str
+    else:
+        info_str = hr("Место на диске".upper(), width=width) + info_str
+
+    if del_video > 0:
+        info_str += hr("Удалите видео".upper(), width=width, color="#ff0000")
+    elif disk_video > 0:
+        info_str += hr("ЗАГРУЗИТЕ ВИДЕО", width=width, color="#0078D7")
+
     return info_str
 
 def get_process_game_info(games: list[Game], stat: Data):
@@ -87,12 +107,15 @@ def get_snowrunner_info(stat: Data, sr_game: Game):
     if count_sr_session <= 0 and count_sr_date <= today():
         pc_info += f"Сегодня SnowRunner: {time_format(sr_game.time_limit)}\n"
         tg_info += f"• Сегодня SnowRunner!!! {time_format(sr_game.time_limit)}\n"
-    elif count_sr_session > 0:
-        pc_info += f"До SnowRunner'а ещё {count_sr_session} сесси{suffix}\n"
-        tg_info += f"• SR: {count_sr_session}"
     else:
-        pc_info += f"SnowRunner после {pc_date_format(count_sr_date)}\n"
-        tg_info += f"• SR: {short_date_format(count_sr_date)}\n"
+        if count_sr_session > 0:
+            pc_info += f"До SnowRunner'а ещё {count_sr_session} сесси{suffix}\n"
+            tg_info += f"• SR: {count_sr_session}"
+        if count_sr_date > today():
+            pc_info += f"SnowRunner после {pc_date_format(count_sr_date)}\n"
+            tg_info += f"• SR: {short_date_format(count_sr_date)}\n"
+
+    pc_info = borders(pc_info, "SnowRunner", sr_game.color)
 
     return {"pc": pc_info, "tg": tg_info}
 
@@ -104,7 +127,7 @@ def get_chance_info(games: list[Game], stat: Data, is_select_forced):
 
     if is_select_forced:
         selected_game = select_game(games, stat, make_selection=False)
-        pc_info += f"Force: {selected_game.full_name}\n"
+        pc_info += f"{selected_game.full_name}\n"
         tg_info += f"• Force: {selected_game.short_name}\n"
     
     if games[0].chance == games[1].chance == 1:
@@ -117,6 +140,14 @@ def get_chance_info(games: list[Game], stat: Data, is_select_forced):
             pc_info += f"{game.full_name}: {game.chance}\n"
             tg_info += f"• {game.short_name}: {game.chance}\n"
 
+    if is_select_forced:
+        border_color = "#ff0000"
+        border_text = "FORCE"
+    else:
+        border_color = get_chance_color(games)
+        border_text = "ШАНС"
+    pc_info = borders(pc_info, border_text=border_text, color=border_color)
+
     return {"pc": pc_info, "tg": tg_info}
 
 def get_time_limit_info(games: list[Game]):
@@ -126,6 +157,8 @@ def get_time_limit_info(games: list[Game]):
         if game.time_limit != 120:
             pc_info += f"{game.full_name}: ::({time_format(game.time_limit)})::\n"
     
+    pc_info = borders(pc_info, border_text="ВРЕМЯ", color=get_time__limit_color(games))
+
     return pc_info
 
 def get_content_time_info(games: list[Game]):
@@ -149,3 +182,52 @@ def get_tg_time_info(games: list[Game]):
             tg_info += "\n"
 
     return tg_info
+
+def hex_to_rgb(hex_color) -> tuple[int, int, int]:
+    hex_color = hex_color.lstrip('#')
+    return tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+
+def color_hex(text: str, hex_color: str) -> str:
+    r, g, b = hex_to_rgb(hex_color)
+    return f"\033[38;2;{r};{g};{b}m{text}\033[0m"
+
+
+def hr(text: str = '', width: int = 42, char: str = '=', color: str = '#ffffff') -> str:
+    if visible_length(text) > width:
+        return color_hex(text, color) + "\n"
+    if text:
+        text = f" {text} "
+        remaining = width - visible_length(text)
+        if remaining < 0:
+            line = text[:width]
+        else:
+            left = remaining // 2
+            right = remaining - left
+            line = char * left + text + char * right
+    else:
+        line = char * width
+
+    return color_hex(line, color) + "\n"
+
+def get_strings_width(strings):
+    return visible_length(max(strings.split("\n"), key=len))
+
+def borders(text: str, border_text:str = '', color = "#ffffff"):
+    width = get_strings_width(text)
+    text = hr(border_text, color=color, width=width) + text
+    text += hr(color=color, width=width)
+
+    return text
+
+def get_chance_color(games: list[Game]):
+    """
+        Возвращает цвет более вероятной игры или белый, если шансы равны
+    """
+    return "#ffffff" if games[0].chance == games[1].chance == 1 else max(games, key=lambda g: g.chance).color
+
+def get_time__limit_color(games: list[Game]):
+    return min(games, key=lambda g: g.time_limit).color
+
+def visible_length(text: str) -> int:
+    ansi_escape = re.compile(r'\033\[[0-9;]*m')
+    return len(ansi_escape.sub('', text))
--- episode choose remake/main.py
+++ episode choose remake/main.py
@@ -1,3 +1,45 @@
+import ctypes
+import os
+
+def set_window_pos(icon_path, title, x, y, width, height):
+    user32 = ctypes.windll.user32
+    kernel32 = ctypes.windll.kernel32
+
+    hwnd = kernel32.GetConsoleWindow()
+
+    # Позиция и размеры
+    user32.MoveWindow(hwnd, x, y, width, height, True)
+
+    # Заголовок
+    user32.SetWindowTextW(hwnd, title)
+
+    # Загружаем иконку из файла
+    IMAGE_ICON = 1
+    LR_LOADFROMFILE = 0x00000010
+    hicon = user32.LoadImageW(
+        0,
+        icon_path,
+        IMAGE_ICON,
+        256,
+        256,
+        LR_LOADFROMFILE
+    )
+
+    WM_SETICON = 0x0080
+    ICON_SMALL = 0
+    ICON_BIG = 1
+
+    # Для заголовка
+    user32.SendMessageW(hwnd, WM_SETICON, ICON_SMALL, hicon)
+    # Для панели задач
+    user32.SendMessageW(hwnd, WM_SETICON, ICON_BIG, hicon)
+
+
+
+
+set_window_pos(r"..\Visual Elements\icon-square.ico", "Episode Chooser", 472, 276, 374, 444)
+os.system('mode con: cols=50 lines=28')
+
 from game import *
 from data import Data
 import paths
@@ -51,7 +93,7 @@ class Main:
         print_info(games, stat, titles)
 
         force_game_id = ""
-        if stat.process_game_id == -1:
+        if stat.process_game_id == -1 and not select_game(games, stat, make_selection=False):
             force_game_id = input("Spin roulette or enter game id: ")
 
         if force_game_id != "":
