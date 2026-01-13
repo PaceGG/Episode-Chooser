d990d8753e285afd845e6b063184ab009bc98a9d
PaceGG <yura3012004@gmail.com>
2026-01-13 07:39:34 +0500

    feat: Страница для планирования

--- react-remake/src/App.jsx
+++ react-remake/src/App.jsx
@@ -1,5 +1,6 @@
 import "./App.css";
 import HomePage from "./HomePage";
+import Schedule from "./schedule/Schedule";
 import MonthlyStats from "./recap/MonthlyStats";
 import Recap from "./recap/Recap";
 import recap from "./recap/recaps/25-11.json";
@@ -13,6 +14,7 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/recap" element={<Recap data={recap} />} />
           <Route path="/stats" element={<MonthlyStats data={recap} />} />
+          <Route path="/plan" element={<Schedule />} />
         </Routes>
       </BrowserRouter>
     </>
---
+++ react-remake/src/schedule/SortableItem.jsx
@@ -0,0 +1,18 @@
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+export function SortableItem({ id, children }) {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id });
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    marginBottom: 8,
+  };
+
+  return (
+    <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+      {children}
+    </div>
+  );
+}
