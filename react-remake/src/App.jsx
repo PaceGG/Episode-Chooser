1444ebff5bbd21091cb814b7daa54ac6010b26d5
PaceGG <yura3012004@gmail.com>
2026-01-13 09:38:01 +0500

    feat: drag and drop component

--- react-remake/src/App.jsx
+++ react-remake/src/App.jsx
@@ -5,6 +5,7 @@ import MonthlyStats from "./recap/MonthlyStats";
 import Recap from "./recap/Recap";
 import recap from "./recap/recaps/25-11.json";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import DragAndDropDemo from "./schedule/Draggable";
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
           <Route path="/" element={<HomePage />} />
           <Route path="/recap" element={<Recap data={recap} />} />
           <Route path="/stats" element={<MonthlyStats data={recap} />} />
-          <Route path="/plan" element={<Schedule />} />
+          {/* <Route path="/plan" element={<Schedule />} /> */}
+          <Route path="/plan" element={<DragAndDropDemo />} />
         </Routes>
       </BrowserRouter>
     </>
