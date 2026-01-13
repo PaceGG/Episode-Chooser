642f68db6740a96dcd3415ec0b31a4701f2a50f4
PaceGG <yura3012004@gmail.com>
2026-01-13 05:40:13 +0500

    chore: Добавлен react-router-dom

--- react-remake/src/App.jsx
+++ react-remake/src/App.jsx
@@ -3,13 +3,18 @@ import HomePage from "./HomePage";
 import MonthlyStats from "./recap/MonthlyStats";
 import Recap from "./recap/Recap";
 import recap from "./recap/recaps/25-11.json";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <HomePage />
-      {/* <MonthlyStats data={recap} /> */}
-      {/* <Recap data={recap} /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/recap" element={<Recap data={recap} />} />
+          <Route path="/stats" element={<MonthlyStats data={recap} />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
